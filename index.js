4140697d1a0a7b6824b0ad1b4260fb16fa96a23d
arsalan_qadir <arsalan.qadir@otwosoft.com>
2023-03-01 15:46:17 +0500

    add server.js and separate server listen in server.js file

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 require("dotenv").config();
 const express = require("express");
-const toursRouter = require("./routes/toursRoutes")
+const toursRouter = require("./routes/toursRoutes");
 const app = express();
 
 app.get("/", (req, res) => {
   res.status(201).json({ message: "hello world" });
 });
 
-
-
 app.use("/api/v1/all-tours", toursRouter);
 
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+module.exports = app;
