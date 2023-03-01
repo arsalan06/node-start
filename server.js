d23a5bddcac0657b951fc5055e492c667fcf1491
arsalan_qadir <arsalan.qadir@otwosoft.com>
2023-03-01 18:00:22 +0500

    add tourSchema and run it

--- server.js
+++ server.js
@@ -13,6 +13,23 @@ mongoose
     console.log(con.connections);
     console.log("db connected successful!");
   });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A tour must have a name"],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price"],
+  },
+});
+const Tour = mongoose.model("Tour", tourSchema);
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
