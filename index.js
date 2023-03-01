791d5990e5bb19ef15c3bd6c1290c47f09ca5f6a
arsalan_qadir <arsalan.qadir@otwosoft.com>
2023-03-01 14:37:19 +0500

    create a node project and simple get api rout

---
+++ index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(201).json({ message: "hello world" });
+});
+const getAllTours = (req, res) => {
+  res.status(201).json({ message: "All Tours Avialable" });
+};
+const postAllTours = (req, res) => {
+  res.status(201).json({ message: "post Tours Avialable" });
+};
+
+// =====> one way to define routes
+
+// app.get("/all-tours", getAllTours);
+// app.get("/all-tours", postAllTours);
+
+
+// =====> second way to define routes
+app.route("/all-tours").get(getAllTours).post(postAllTours);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
