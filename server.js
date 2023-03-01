bb96623859426e138778aa62370a2dd808c580b3
arsalan_qadir <arsalan.qadir@otwosoft.com>
2023-03-01 18:14:09 +0500

    add data in Tour model with static way or with static object

--- server.js
+++ server.js
@@ -30,6 +30,19 @@ const tourSchema = new mongoose.Schema({
   },
 });
 const Tour = mongoose.model("Tour", tourSchema);
+const testTour = new Tour({
+  name: "The Forest Hiker",
+  rating: 4.7,
+  price: 555,
+});
+testTour
+  .save()
+  .then((document) => {
+    console.log(document);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
