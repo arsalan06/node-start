08a3c2639626103200b10427026d5c5e3af976fa
arsalan_qadir <arsalan.qadir@otwosoft.com>
2023-03-01 17:41:47 +0500

    add mongoose and connect to local DB successfully

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 require("dotenv").config();
-const app =require("./index")
+const mongoose = require("mongoose");
+const app = require("./index");
+const DB = process.env.DATABASE_URI;
+console.log(DB);
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log("db connected successful!");
   });
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
