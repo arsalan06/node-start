22a37191b951590eaf2c9853ce1c93077c2ef7ef
arsalan_qadir <arsalan.qadir@otwosoft.com>
2023-03-02 18:26:39 +0500

    update tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,24 +1,60 @@
 const mongoose = require("mongoose");
 
 const tourSchema = new mongoose.Schema({
-    name: {
-      type: String,
-      required: [true, "A tour must have a name"],
-      unique: true,
-    },
-    rating: {
-      type: Number,
-      default: 4.5,
-    },
-    price: {
-      type: Number,
-      required: [true, "A tour must have a price"],
-    },
-  });
-  const Tour = mongoose.model("Tour", tourSchema);
-  //=====> for static data insert in model
+  name: {
+    type: String,
+    required: [true, "A tour must have a name"],
+    unique: true,
+    trim: true,
+  },
+  duration: {
+    type: Number,
+    required: [true, "A tour must have duration"],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "A tour must have a group size"],
+  },
+  difficulty: {
+    type: String,
+    required: [true, "A tour must have difficulty"],
+  },
+  ratingAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingQuantity: {
+    type: Number,
+    default: 0,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price"],
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, "A tour must have a summary"],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, "A tour must have a cover image"],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
+});
+const Tour = mongoose.model("Tour", tourSchema);
+//=====> for static data insert in model
 
-  
 //   const testTour = new Tour({
 //     name: "The Park",
 //   //   rating: 4.7,
