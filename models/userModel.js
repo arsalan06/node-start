1ac69494d9fdc006dd30b72830e577842bc83472
arsalan_qadir <arsalan.qadir@otwosoft.com>
2023-03-10 17:39:22 +0500

    add only user model structure

---
+++ models/userModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const slugify = require("slugify");
+const validator = require("validator");
+//name, email, photo, password, passwordConfirm
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "please tell us your name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please tell us your email"],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "please provide valid email"],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, "please provide a password"],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "please provide a comfirm password"],
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
