ab2a91ab6a96e6d1c051b67cab57aa5554379820
arsalan_qadir <arsalan.qadir@otwosoft.com>
2023-03-12 21:29:02 +0500

    add a globel midlware in index file for error handling

--- index.js
+++ index.js
@@ -8,12 +8,24 @@ app.get("/", (req, res) => {
   res.status(201).json({ message: "hello world" });
 });
 
-app.use("/api/v1/all-tours", toursRouter);
+// app.use("/api/v1/all-tours", toursRouter);
 app.use("/api/v1/tours", toursRouter);
-app.all("*", (req, res)=>{
-  res.status({
-    status:"fail",
-    message:`can't find ${req.originalUrl} on this server`
+app.all("*", (req, res, next)=>{
+  // res.status(404).json({
+  //   status:"fail",
+  //   message:`can't find ${req.originalUrl} on this server`
+  // })
+  const err =new Error(`can't find ${req.originalUrl} on this server`);
+  err.status="fail";
+  err.statusCode=404;
+  next(err)
+})
+app.use((err, req, res, next)=>{
+  err.statusCode=err.statusCode || 500
+  err.status=err.status || "error"
+  res.status(err.statusCode).json({
+    status: err.status,
+    message:err.message
   })
 })
 module.exports = app;
