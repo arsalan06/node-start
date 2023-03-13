5f7bb45697fbc6479cafd77f5c71c0a8e3232208
arsalan_qadir <arsalan.qadir@otwosoft.com>
2023-03-13 12:31:21 +0500

    add password authentication and password hashing

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 //name, email, photo, password, passwordConfirm
 
 const userSchema = new mongoose.Schema({
@@ -24,9 +25,24 @@ const userSchema = new mongoose.Schema({
   confirmPassword: {
     type: String,
     required: [true, "please provide a comfirm password"],
+    validate: {
+      // This will only work on save
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "passwords are not same!",
+    },
   },
 });
+userSchema.pre("save", async function (next) {
+  // this only run this function when password update or modified
+  if (!this.isModified("password")) return next();
 
+  //Hash password
+  this.password = await bcrypt.hash(this.password, 12);
+  this.confirmPassword = undefined;
+  next()
+});
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
